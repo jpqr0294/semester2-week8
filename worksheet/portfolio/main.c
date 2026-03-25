4a1771831ad8d0765244ac8b646dd3416b7699e0
jpqr0294 <jpqr0294@leeds.ac.uk>
2026-03-25 20:51:58 +0000

    worksheet 2.8

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -7,8 +7,34 @@
 int main( void ) {
 
     Point p1 = makePoint(0.0,0.0);
-    
+    Point p2 = makePoint(5.5, 6.7);
+    Point p3 = makePoint(10.1, 12.4);
+    Point p4 = makePoint(20.0, 40.0);
+    Point p5 = makePoint(5.5, 6.7);
+
     // test your code by calling the functions and printing the output 
+    Line l1 = makeLine(p1, p2);
+    Line l2 = makeLine(p2, p3);
+    Line l3 = makeLine(p3, p4);
+
+    Triangle t1 = makeTriangle(p1, p2, p3);
+    Triangle t2 = makeTriangle(p2, p3, p4);
+
     
+
+    printf("length of line 1 = %f\n", lineLength(l1));
+    printf("length of line 2 = %f\n", lineLength(l2));
+    printf("length of line 3 = %f\n", lineLength(l3));
+
+    printf("area of triangle 1 = %f\n", triangleArea(t1));
+    printf("area of triangle 2 = %f\n", triangleArea(t2));
+    
+    printf("is p2 same point as p5?: %s\n", samePoint(p2, p5) ? "true" : "false");
+
+    printf("is p2 in the line l2?: %s\n", pointInLine(p2, l2) ? "true" : "false");
+    printf("is p2 in the line l3?: %s\n", pointInLine(p2, l3) ? "true" : "false");
+
+    printf("is p1 in the triangle t1?: %s\n", pointInTriangle(p1, t1) ? "true" : "false");
+    printf("is p1 in the triangle t2?: %s\n", pointInTriangle(p1, t2) ? "true" : "false");
     return 0;
 }
--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -6,7 +6,8 @@
 
 Point makePoint( float x, float y ) {
     Point new;
-    // implementation
+    new.x = x;
+    new.y = y;
     return new;
 }
 
@@ -14,3 +15,67 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+
+Line makeLine( Point p1, Point p2) {
+    Line lin;
+    lin.p[0] = p1;
+    lin.p[1] = p2;
+    return lin;
+}
+
+
+Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
+    Triangle tri;
+    tri.p[0] = p1;
+    tri.p[1] = p2;
+    tri.p[2] = p3;
+    return tri;
+}
+
+
+float lineLength( Line l ) {
+    float length = 0.0;
+    float dx = 0.0;
+    float dy = 0.0;
+
+    dx = l.p[1].x - l.p[0].x;
+    dy = l.p[1].y - l.p[0].y;
+    length = sqrtf(dx*dx + dy*dy);
+
+    return length;
+}
+
+float triangleArea( Triangle t ) {
+    float area = 0.0;
+    float x1 = t.p[0].x, y1 = t.p[0].y;
+    float x2 = t.p[1].x, y2 = t.p[1].y;
+    float x3 = t.p[2].x, y3 = t.p[2].y;
+
+    area = 0.5f * fabsf(
+        x1*(y2-y3) +
+        x2*(y3-y1) +
+        x3*(y1-y2)
+    );
+
+    return area;
+}
+
+
+bool samePoint( Point p1, Point p2 ) {
+    float dist = lineLength(makeLine(p1,p2));
+    
+    return dist<1.0e-6;
+}
+
+
+bool pointInLine( Point p, Line l) {
+    return samePoint(p, l.p[0]) || 
+            samePoint(p, l.p[1]);
+}
+
+
+bool pointInTriangle( Point p, Triangle t ) {
+    return samePoint(p, t.p[0]) ||
+            samePoint(p, t.p[1]) || 
+            samePoint(p, t.p[2]);
+}
